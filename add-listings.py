4e61099349e2e26204189a3e23d402bf38956172
P. F. Chimento <philip.chimento@gmail.com>
2011-03-26 15:31:41 +0100

    Strip indentation from inserted code blocks
    When inserting a block of code from a file, if there is indentation
    common to all lines, strip it, so that the outermost indentation level
    is zero.

--- add-listings.py
+++ add-listings.py
@@ -22,7 +22,14 @@ for line in fileinput.input():
 		else:
 			end = start + 1
 
+		lines = lines[start:end]
+
+		# Remove any indentation common to the whole block
+		while all([l.startswith(' ') for l in lines if l != '\n']) \
+			or all([l.startswith('\t') for l in lines if l != '\n']):
+			lines = [l[1:] for l in lines if l != '\n']
+
 		print '<code mime="text/x-csrc"><![CDATA['
-		for l in lines[start:end]:
+		for l in lines:
 			print l,
 		print ']]></code>'
