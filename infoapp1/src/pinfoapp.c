ee769bf11f50c3239b220be085f7d8ae5c4ab250
Philip Chimento <philip.chimento@gmail.com>
2011-09-11 01:54:38 +0200

    Changes to infoapp
    Make application-id valid, and implement the 'activate' signal; these
    are required for GApplication to work

--- infoapp1/src/pinfoapp.c
+++ infoapp1/src/pinfoapp.c
@@ -5,6 +5,8 @@
 
 G_DEFINE_TYPE(PInfoApp, p_info_app, G_TYPE_APPLICATION);
 
+static void p_info_app_activate(GApplication *theapp);
+
 static void
 p_info_app_init(PInfoApp *self)
 {
@@ -13,6 +15,8 @@ p_info_app_init(PInfoApp *self)
 static void
 p_info_app_class_init(PInfoAppClass *klass)
 {
+	GApplicationClass *application_class = G_APPLICATION_CLASS(klass);
+	application_class->activate = p_info_app_activate;
 }
 
 PInfoApp *
@@ -22,9 +26,14 @@ p_info_app_get(void)
 
 	if(G_UNLIKELY(theapp == NULL))
 		theapp = P_INFO_APP(g_object_new(P_TYPE_INFO_APP,
-			"application-id", "org.ptomato.app-skeleton",
+			"application-id", "org.ptomato.infoapp",
 			NULL));
 
 	return theapp;
 }
 
+static void
+p_info_app_activate(GApplication *theapp)
+{
+	g_print("Activate\n");
+}
